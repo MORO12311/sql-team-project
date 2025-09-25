748383b11a2d86b808b814202a867d73fb609503
MORO12311 <154279787+MORO12311@users.noreply.github.com>
2025-09-25 20:22:13 +0300

    Update KPIs Views.sql Edit Nouran code block
    messy code

--- scripts/KPIs Views.sql
+++ scripts/KPIs Views.sql
@@ -322,20 +322,148 @@ CREATE VIEW rpc AS
 
 -- _______________________________________________Nour_____________________________________________________
 -- 3. Orders and Revenue
+DROP VIEW IF EXISTS total_orders;
+CREATE VIEW total_orders AS
+    SELECT 
+        COUNT(order_id) AS total_orders
+    FROM
+        orders;
+-- Revenue and Net Revenue
+DROP VIEW IF EXISTS revenue_summary;
 
--- Net Revenue
-    DROP VIEW IF EXISTS net_revenue;
-    CREATE VIEW net_revenue AS
-      SELECT SUM(oi.price_usd) - SUM(oir.refund_amount_usd) AS net_revenue
-      FROM order_items AS oi
-      LEFT JOIN order_item_refunds AS oir ON oi.order_item_id = oir.order_item_id;
+CREATE VIEW revenue_summary AS
+SELECT 
+    SUM(oi.price_usd)                        AS total_revenue,
+    SUM(oi.price_usd) - SUM(oir.refund_amount_usd) AS net_revenue
+FROM order_items AS oi
+LEFT JOIN order_item_refunds AS oir 
+    ON oi.order_item_id = oir.order_item_id;
 
--- Gross Merchandise Value (GMV)
-    DROP VIEW IF EXISTS gmv;
-    CREATE VIEW gmv AS
-      SELECT SUM(price_usd) AS volume_of_transactions
-      FROM order_items;
 
+-- Gross Merchandise Value (GMV) 
+DROP VIEW IF EXISTS GMV;
+CREATE VIEW GMV AS
+    SELECT 
+        SUM(price_usd) AS volume_of_transactions
+    FROM
+        order_items;
+
+    -- Average Order Value (AOV) 
+DROP VIEW IF EXISTS Average_order_value;
+CREATE VIEW Average_order_value AS
+    SELECT 
+        SUM(price_usd) / COUNT(order_id) AS Average_Order_Value
+    FROM
+        orders;
+
+-- Gross Margin 
+DROP VIEW IF EXISTS Gross_margin;
+CREATE VIEW Gross_margin AS
+    SELECT 
+        CONCAT(ROUND(((SUM(price_usd) - SUM(cogs_usd)) / SUM(price_usd)) * 100,
+                        2),
+                '%') AS gross_margin
+    FROM
+        orders;
+
+-- Refund/Return Rate by orders
+DROP VIEW IF EXISTS return_rate_by_orders;
+CREATE VIEW return_rate_by_orders AS
+SELECT 
+    ROUND(COUNT(DISTINCT (oir.order_id)) / COUNT(DISTINCT (o.order_id)) * 100,
+            3) AS Return_rate_by_orders
+FROM
+    orders AS o
+        LEFT JOIN
+    order_item_refunds AS oir ON o.order_id = oir.order_id;
+
+-- Refund/Return Rate by Value
+DROP VIEW IF EXISTS return_rate_by_value;
+CREATE VIEW return_rate_by_value AS
+SELECT
+CONCAT( 
+    ROUND(
+        (SUM(oir.refund_amount_usd) * 1.0 / SUM(o.price_usd)) * 100,
+    2),'%' )AS return_rate_value
+FROM orders o
+LEFT JOIN order_item_refunds as oir
+    ON o.order_id = oir.order_id;
+
+-- Refund Rates by Product
+DROP VIEW IF EXISTS return_rate_by_products;
+CREATE VIEW return_rate_by_products AS
+SELECT 
+    p.product_id,
+    p.product_name,
+    COUNT(DISTINCT oir.order_item_id) AS returned_items,
+    COUNT(DISTINCT oi.order_item_id) AS total_items_sold,
+    CONCAT(
+    ROUND(
+        (COUNT(DISTINCT oir.order_item_id) * 1.0 / COUNT(DISTINCT oi.order_item_id)) * 100,
+        2
+    ) ,'%') AS return_rate_percent
+FROM products AS p
+LEFT JOIN order_items AS oi 
+    ON p.product_id = oi.product_id
+LEFT JOIN order_item_refunds AS oir
+    ON oi.order_item_id = oir.order_item_id
+GROUP BY p.product_id, p.product_name
+ORDER BY return_rate_percent DESC;
+
+-- orders by product 'no of orders that has the product'
+DROP VIEW IF EXISTS orders_by_product;
+CREATE VIEW orders_by_product AS
+SELECT 
+    p.product_id,
+    p.product_name,
+    COUNT(DISTINCT oi.order_id) AS number_of_orders
+FROM
+    products AS p
+        LEFT JOIN
+    order_items AS oi ON oi.product_id = p.product_id
+GROUP BY p.product_id , p.product_name;
+
+-- Revenue by product
+DROP VIEW IF EXISTS Rev_by_product;
+CREATE VIEW Rev_by_product AS
+    SELECT 
+        p.product_id,
+        p.product_name,
+        SUM(oi.price_usd) AS Revenues_usd
+    FROM
+        products AS p
+            LEFT JOIN
+        order_items AS oi ON p.product_id = oi.product_id
+    GROUP BY p.product_id , p.product_name;
+
+ -- Revenue by margin  
+ DROP VIEW IF EXISTS rev_by_margin;
+create view rev_by_margin as
+    SELECT 
+    p.product_id,
+    p.product_name,
+    SUM(oi.price_usd - oi.cogs_usd) AS margin_usd
+FROM products p
+JOIN order_items oi 
+    ON p.product_id = oi.product_id
+GROUP BY p.product_id, p.product_name;
+
+-- Product Portfolio Impact 
+DROP VIEW IF EXISTS Product_Portfolio_Impact;
+create view Product_Portfolio_Impact as
+SELECT 
+    p.product_id,
+    p.product_name,
+    SUM(oi.price_usd) AS revenue_usd,
+   CONCAT(
+		ROUND( (SUM(oi.price_usd) * 100.0) / SUM(SUM(oi.price_usd)) OVER(), 2) ,'%') AS contribution_percent
+FROM products p
+JOIN order_items oi 
+    ON p.product_id = oi.product_id
+GROUP BY p.product_id, p.product_name
+ORDER BY revenue_usd DESC;
+
+-- _______________________________________________Faris_____________________________________________________
 -- CAGR Calculation
     DROP VIEW IF EXISTS cagr_calc;
     CREATE VIEW cagr_calc AS
